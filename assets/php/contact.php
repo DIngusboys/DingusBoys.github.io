33bd0493e80fcffc7dd3ed33d242b7a743126fcf
DIngusboys <corbetarcher@ymail.com>
2017-08-18 22:19:17 -0400

    qwe
    qwe

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,64 +1,37 @@
 <?php
   require 'vendor/phpmailer/PHPMailerAutoload.php';
 
-//
-// config
-// --------------------------------------------------
-//
+  $mail = new PHPMailer;
 
-  // your email address
-  $EMAIL   = 'DingusBoys@yahoo.com';
+  //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
-  // your name
-  $NAME    = 'DingusBoys';
+  $mail->isSMTP();                                      // Set mailer to use SMTP
+  $mail->Host = 'smtp.mail.yahoo.com';  // Specify main and backup SMTP servers
+  $mail->SMTPAuth = true;                               // Enable SMTP authentication
+  $mail->Username = 'dingusboys@gmail.com';                 // SMTP username
+  $mail->Password = 'Qweqweqwe1!';                           // SMTP password
+  $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+  $mail->Port = 465;                                    // TCP port to connect to
 
-  // subject line
-  $SUBJECT = 'Support Team';
+  $mail->setFrom('from@example.com', 'Mailer');
+  $mail->addAddress('dingusboys@yahoo.com', 'Joe User');     // Add a recipient
+  $mail->addAddress('ellen@example.com');               // Name is optional
+  $mail->addReplyTo('info@example.com', 'Information');
+  $mail->addCC('cc@example.com');
+  $mail->addBCC('bcc@example.com');
 
-  // email content
-  $HTML = '
-    <html>
-      <head>
-        <title>' . $SUBJECT . '</title>
-      </head>
-      <body>
-        <p><strong style="width: 80px;">Name: </strong>' . $_POST['name'] . '</p>
-        <p><strong style="width: 80px;">Email: </strong>' . $_POST['email'] . '</p>
-        <p><strong style="width: 80px;">Message: </strong>' . $_POST['message'] . '</p>
-      </body>
-    </html>
-  ';
+  $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+  $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+  $mail->isHTML(true);                                  // Set email format to HTML
 
-//
-// script
-// --------------------------------------------------
-//
+  $mail->Subject = 'Here is the subject';
+  $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+  $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-  if($_SERVER['REQUEST_METHOD'] != 'POST') {
-    die('An error occurred. Please try again later.'); // no html
+  if(!$mail->send()) {
+      echo 'Message could not be sent.';
+      echo 'Mailer Error: ' . $mail->ErrorInfo;
   } else {
-    if (empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
-      die('Only allow access via AJAX'); // no html
-    } else {
-      if (empty($EMAIL) || empty($NAME) || empty($SUBJECT) || empty($HTML)) {
-        die(json_encode(array('type' => 'error', 'msg' => '<i class="error"></i> An error occurred. Please check php config field.')));
-      } else {
-        extract($_POST, EXTR_PREFIX_ALL, 'form');
-
-        $mail = new PHPMailer(true);
-        $mail->CharSet = 'UTF-8';
-        $mail->setFrom($form_email, $form_name);
-        $mail->addReplyTo($form_email, $form_name);
-        $mail->addAddress($EMAIL, $NAME);
-        $mail->Subject = $SUBJECT;
-        $mail->msgHTML($HTML);
-
-        if (!$mail->send()) {
-          die(json_encode(array('type' => 'error', 'msg' => '<i class="error"></i> An error occurred. Please try again later.')));
-        } else {
-          die(json_encode(array('type' => 'success', 'msg' => '<i class="success"></i> Your message has been sent.')));
-        }
-      }
-    }
+      echo 'Message has been sent';
   }
 ?>
